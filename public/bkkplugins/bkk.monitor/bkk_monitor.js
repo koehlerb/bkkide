e1d1c95d523e27618dcfeb24e122700ecdbccd04
Brian Koehler <brian@koehler.ca>
2018-12-20 09:27:41 -0800

    basic edit monitoring

--- public/bkkplugins/bkk.monitor/bkk_monitor.js
+++ public/bkkplugins/bkk.monitor/bkk_monitor.js
@@ -8,10 +8,17 @@ define(function(require, exports, module) {
     function main(options, imports, register) {
         var Plugin = imports.Plugin;
         var ace = imports.ace;
-        
+
         var plugin = new Plugin("Ajax.org", main.consumes);
 
         var foobar = 1;
+
+        function regChangeHandler( aceSession ) {
+          aceSession.doc.on( "change", function( delta ) {
+            console.log( "*** A document changed!" );
+            console.log( delta );
+          }, true );
+        }
 
         function load() {
           console.log( "bkk.monitor load");
@@ -19,6 +26,7 @@ define(function(require, exports, module) {
               var doc = e.doc;
               var path = doc.tab.path;
               console.log( "*** initAceSession: " + path );
+              regChangeHandler( doc.getSession().session );
               //var otDoc = documents[path];
               //if (otDoc && !otDoc.session)
               //    otDoc.setSession(doc.getSession().session);
